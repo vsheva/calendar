0c77864415a8a577580c07aaaefdff49d8bdc989
vsheva <valera.sheva@gmail.com>
2022-06-01 03:45:17 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,92 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Header from './components/header/Header.jsx';
 import Calendar from './components/calendar/Calendar.jsx';
-
 import { getWeekStartDate, generateWeekRange } from '../src/utils/dateUtils.js';
-
 import './common.scss';
 
-class App extends Component {
-  state = {
-    weekStartDate: new Date(),
-  };
 
-  render() {
-    const { weekStartDate } = this.state;
-    const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
+const App =()=> {
+  const[currentWeek, setCurrentWeek]= useState(new Date())
+  const weekDates = generateWeekRange(getWeekStartDate(currentWeek));
 
-    return (
+  const todayWeek=()=>setCurrentWeek(new Date())
+  const prevWeek=()=>setCurrentWeek(new Date(currentWeek.setDate(currentWeek.getDate()-7)))
+  const nextWeek=()=>setCurrentWeek(new Date(currentWeek.setDate(currentWeek.getDate()+7)))
+
+
+
+  return (
       <>
-        <Header />
-        <Calendar weekDates={weekDates} />
+        <Header weekDates={weekDates}
+                todayWeek={todayWeek}
+                prevWeek={prevWeek}
+                nextWeek={nextWeek}
+        />
+
+        <Calendar weekDates={weekDates}
+
+        />
       </>
-    );
-  }
+  )
 }
 
+
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const date =new Date
+console.log(new Date(date.setDate(date.getDate() - 7)))  //Wed May 25 2022 02:46:40 GMT+0300 (Eastern European Summer Time)
+
+console.log(date.getDate())                          //1 - число от 1 до 31 месяца
+console.log(date.getMonth())                         //5 -Целое число от 0 до 11, обозначающее месяц указанной даты по местному времени. 0 соответствует январю, 1 — февралю и так далее.
+console.log(date.getDate() - 7)                      //-6
+console.log(date.setDate(date.getDate() - 7))        //1653435857030 -число милисекунд с 1.01.1970
+console.log(date.setDate(25))                        //1653435857030
+
+console.log(new Date(date.setDate(date.getDate() - 7)))   //Wed May 18 2022 02:45:19 GMT+0300 (Eastern European Summer Time)
+console.log(new Date(date.setDate(date.getDate() - 7)))   //Wed May 11 2022 02:49:03 GMT+0300 (Eastern European Summer Time)
+console.log(new Date(date.setDate(-6)))                   //Sun Apr 24 2022 02:49:27 GMT+0300 (Eastern European Summer Time)
+*/
+
+
+
+
+
+
+
+
+
+
+//setState
+
+// class App extends Component {
+//   state = {
+//     weekStartDate: new Date(),
+//   };
+//
+//   render() {
+//     const { weekStartDate } = this.state;
+//     const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
+//     return (
+//       <>
+//         <Header />
+//         <Calendar weekDates={weekDates} />
+//       </>
+//     );
+//   }
+// }
+//
+// export default App;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,25 +1,73 @@
 import React from 'react';
-
 import './header.scss';
 
-const Header = () => {
+import {months} from "./../../utils/dateUtils.js"
+
+
+const Header = ({weekDates, todayWeek, prevWeek, nextWeek}) => {
+
+const firstDayOfCurrentWeek =weekDates[0];
+const lastDayOfCurrentWeek = weekDates[weekDates.length - 1];
+const monthFirstDayOfCurrentWeek = weekDates[0].getMonth()
+const monthLastDayOfCurrentWeek =weekDates[weekDates.length - 1].getMonth()
+
+const displayedMonth = firstDayOfCurrentWeek === lastDayOfCurrentWeek
+    ? months[monthFirstDayOfCurrentWeek] : months[monthLastDayOfCurrentWeek]
+
+
   return (
     <header className="header">
-      <button className="button create-event-btn">
-        <i className="fas fa-plus create-event-btn__icon"></i>Create
+      <button className="button create-event-btn" >
+        <i className="fas fa-plus create-event-btn__icon" ></i>Create
       </button>
       <div className="navigation">
-        <button className="navigation__today-btn button">Today</button>
+        <button className="navigation__today-btn button" onClick={todayWeek}>Today</button>
         <button className="icon-button navigation__nav-icon">
-          <i className="fas fa-chevron-left"></i>
+          <i className="fas fa-chevron-left" onClick={prevWeek}></i>
         </button>
         <button className="icon-button navigation__nav-icon">
-          <i className="fas fa-chevron-right"></i>
+          <i className="fas fa-chevron-right" onClick={nextWeek}></i>
         </button>
-        <span className="navigation__displayed-month"></span>
+        <span className="navigation__displayed-month">{displayedMonth}</span>
       </div>
     </header>
   );
 };
 
 export default Header;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const monthFirstDayOfCurrentWeek = weekDates[0].getMonth();
+// const monthLastDayOfCurrentWeek = weekDates[weekDates.length - 1].getMonth()
+//
+// const monthDiplayed = (monthFirstDayOfCurrentWeek === monthLastDayOfCurrentWeek)
+//     ? (months[monthFirstDayOfCurrentWeek])
+//     : (months[monthLastDayOfCurrentWeek])
+//
+//
+
+
+
+
+
+
