e6d03733e30d8f555a31c25ff38ddb46e9db44c9
vsheva <valera.sheva@gmail.com>
2023-07-25 23:07:45 -0400

    fix

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { days } from '../../utils/dateUtils.js';
-import classNames from "classNames"
+import classNames from 'classnames';
 import "./navigation.scss"
 
 const Navigation = ({ weekDates }) => {
